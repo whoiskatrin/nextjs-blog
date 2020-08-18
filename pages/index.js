3b5893a55df276f7c589f9aacc6f59fce564d18e
whoiskatrin <katjareznikova@gmail.com>
2020-08-18 19:36:50 +0100

    updated with the balance

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,29 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
+const fetch = require('node-fetch');
+
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const res = await fetch('http://localhost:3000/api/balance')
+  const balance = await res.json()
   return {
     props: {
-      allPostsData
+      balance
     }
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ balance }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Making things happen 🛠👁⚡️</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-  <Link href="/posts/[id]" as={`/posts/${id}`}>
-    <a>{title}</a>
-  </Link>
-  <br />
-  <small className={utilStyles.lightText}>
-    <Date dateString={date} />
-  </small>
-</li>
-          ))}
-        </ul>
+        Your current balance: {balance.text}
       </section>
     </Layout>
   )
