3c990e96ce43d423eec2eed2f5d8ad86f9d562a9
whoiskatrin <katjareznikova@gmail.com>
2020-08-20 08:00:43 +0100

    auth with github draft

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/global.css'
+import { Provider } from 'next-auth/client'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App ({ Component, pageProps }) {
+  const { session } = pageProps;
+  return (
+    <Provider options={{ site: process.env.SITE }} session={session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
+import React from 'react'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import Date from '../components/date'
 import fetch from 'node-fetch'
 import { server } from '../config';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export async function getServerSideProps() {
   const res = await fetch(`${server}/api/balance`)
@@ -13,13 +13,23 @@ export async function getServerSideProps() {
 }
 
 export default function Home(balance) {
+  const [ session, loading ] = useSession()
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-      <h1>  Your current balance: {balance.data.text}</h1>
+      {!session && <>
+        Not signed in <br/>
+        <button onClick={signIn}>Sign in</button>
+      </>}
+      {session && <>
+        Signed in as {session.user.email} <br/>
+        <h1>  Your current balance: {balance.data.text}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </>}
       </section>
     </Layout>
   )
